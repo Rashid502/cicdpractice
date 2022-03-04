810b103fdeb16772e2bd47581a5c4e4ccd08802b
Rashid Pervaiz <rashidpervaiz502@gmail.com>
2022-03-04 15:32:22 +0500

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders CICD Practice', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/CICD practice/i);
   expect(linkElement).toBeInTheDocument();
 });
